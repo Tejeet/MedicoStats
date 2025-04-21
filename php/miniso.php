330608b7de09501597b9b8358e96bee76edf06d6
tejeet <tejeet.magar@nobroker.in>
2025-04-21 17:08:05 +0530

    Added MiniO

--- php/miniso.php
+++ php/miniso.php
@@ -11,41 +11,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload'])) {
     $fileSize = $_FILES['upload']['size'];
     $fileType = $_FILES['upload']['type'];
 
+    if (!file_exists($fileTempPath)) {
+        echo "<p style='color: red;'>Temporary file does not exist.</p>";
+        exit;
+    }
+
+    $fileHandle = fopen($fileTempPath, 'rb');
+    if (!$fileHandle) {
+        echo "<p style='color: red;'>Failed to open temporary file.</p>";
+        exit;
+    }
+
     // Generate S3 signature
     $date = gmdate('D, d M Y H:i:s T');
     $contentType = $fileType;
-    $resourcePath = "/$bucketName/$fileName";
+    $encodedFileName = rawurlencode($fileName);
+    $resourcePath = "/$bucketName/$encodedFileName";
     
     $stringToSign = "PUT\n\n$contentType\n$date\n$resourcePath";
     $signature = base64_encode(hash_hmac('sha1', $stringToSign, $secretKey, true));
 
+    $uploadUrl = "$minioEndpoint$resourcePath";
+
     // Upload to MinIO
     $ch = curl_init();
     curl_setopt_array($ch, [
-        CURLOPT_URL => "$minioEndpoint$resourcePath",
+        CURLOPT_URL => $uploadUrl,
         CURLOPT_CUSTOMREQUEST => "PUT",
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_INFILE => fopen($fileTempPath, 'rb'),
+        CURLOPT_INFILE => $fileHandle,
         CURLOPT_INFILESIZE => $fileSize,
         CURLOPT_HTTPHEADER => [
             "Date: $date",
             "Content-Type: $contentType",
             "Authorization: AWS $accessKey:$signature"
         ],
-        CURLOPT_TIMEOUT => 10
+        CURLOPT_TIMEOUT => 20
     ]);
 
     $response = curl_exec($ch);
     $error = curl_error($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
+    fclose($fileHandle);
 
     if ($error) {
         echo "<p style='color: red;'>cURL Error: $error</p>";
-    } elseif ($httpCode === 200) {
+    } elseif ($httpCode === 200 || $httpCode === 204) {
         echo "<p style='color: green;'>File uploaded successfully: <strong>$fileName</strong></p>";
     } else {
         echo "<p style='color: red;'>MinIO Error (HTTP $httpCode)</p>";
+        echo "<pre>$response</pre>";
     }
 }
 ?>
@@ -66,18 +82,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['upload'])) {
     <h3>Files in Bucket:</h3>
     <ul>
         <?php
+        // List bucket contents
         $ch = curl_init("$minioEndpoint/$bucketName/");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
+        $listResponse = curl_exec($ch);
         curl_close($ch);
 
-        if ($response) {
+        if ($listResponse) {
             libxml_use_internal_errors(true);
-            $xml = simplexml_load_string($response);
+            $xml = simplexml_load_string($listResponse);
             if ($xml !== false) {
                 foreach ($xml->Contents as $content) {
                     $fileName = (string)$content->Key;
-                    $fileUrl = "$minioEndpoint/$bucketName/$fileName";
+                    $fileUrl = "$minioEndpoint/$bucketName/" . rawurlencode($fileName);
                     echo "<li><a href='$fileUrl' target='_blank'>$fileName</a></li>";
                 }
             } else {
